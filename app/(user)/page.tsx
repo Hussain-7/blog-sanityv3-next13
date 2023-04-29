31982bf85630572802f1ef050d2af11c08291bd9
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-04-29 16:00:46 +0500

    banner added

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,13 +1,7 @@
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 const HomePage = ({ props }: any) => {
-  console.log("print data", props);
-  return (
-    <div>
-      <p className="text-2xl font-bold">Home Page</p>
-      <p>Some text</p>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default HomePage;
---
+++ components/Banner.tsx
@@ -0,0 +1,22 @@
+const Banner = () => {
+  return (
+    <div className="flex flex-col lg:flex-row lg:space-x-5 lg:items-center justify-between font-bold px-10 py-5 mb-10">
+      <div>
+        <h1 className="text-5xl sm:text-7xl">Hussain's Daily Blog</h1>
+        <h2 className="mt-5 md:mt-5">
+          Welcome to {""}
+          <span className="underline decoration-4 decoration-[#f7ab0a] ">
+            Every Developers
+          </span>{" "}
+          favourite blog in the Devosphere
+        </h2>
+      </div>
+      <p className="mt-5 md:mt-2 text-gray-400 max-w-md">
+        New product features | The latest in technology | The weekly debugging
+        nightmare & More!
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
