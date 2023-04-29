1de373613d0817272d2a884548766e4fedd28932
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-04-29 15:09:36 +0500

    sanity studio customizations done

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
-const Logo = () => {
-  return <div>Logo</div>;
+const Logo = (props: any) => {
+  const { renderDefault, title } = props;
+  return (
+    <div className="flex items-center space-x-2">
+      <Image
+        src={"/logo.png"}
+        className="rounded-full object-cover"
+        height={50}
+        width={50}
+        alt={"Logo"}
+      />
+      <div className="text-lg font-bold px-3">Blog Studio</div>
+    </div>
+  );
 };
 
 export default Logo;
